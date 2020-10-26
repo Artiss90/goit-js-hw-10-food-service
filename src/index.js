523600be6137687bc8c87598f7416c7ed104a2da
Art <Petrenkoartik@gmail.com>
2020-10-27 00:11:14 +0200

    Test toggle Theme

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { Theme } from './js/theme';
 import './styles.css';
 
-// console.log(Theme);
+console.log(Theme.LIGHT);
+
+const bodyRef = document.querySelector('body');
+const checkboxSwitchThemeRef = document.querySelector('#theme-switch-toggle');
+
+console.log(checkboxSwitchThemeRef);
+
+const toggleTheme = () => { bodyRef.classList.toggle(Theme.DARK) };
+
+checkboxSwitchThemeRef.addEventListener('change', toggleTheme);
--- src/js/theme.js
+++ src/js/theme.js
@@ -4,3 +4,4 @@ const Theme = {
 };
 
 export { Theme };
+
