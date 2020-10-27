f965a3dbe55389ecbd00dc520e57e096a5026ac3
Art <Petrenkoartik@gmail.com>
2020-10-28 00:09:16 +0200

    Закончил изменение и сохранение темы

--- src/index.js
+++ src/index.js
@@ -6,8 +6,30 @@ console.log(Theme.LIGHT);
 const bodyRef = document.querySelector('body');
 const checkboxSwitchThemeRef = document.querySelector('#theme-switch-toggle');
 
-console.log(checkboxSwitchThemeRef);
+const saveThemeLocal = (e) => {
+    if (e.target.checked) { toggleInThemeDark(), localStorage.setItem('theme', Theme.DARK) }
+    else { toggleInThemeLight(), localStorage.setItem('theme', Theme.LIGHT) }
+};
 
-const toggleTheme = () => { bodyRef.classList.toggle(Theme.DARK) };
+/* Вешаем событие переключения темы */
+checkboxSwitchThemeRef.addEventListener('change', saveThemeLocal);
 
-checkboxSwitchThemeRef.addEventListener('change', toggleTheme);
+const loadThemeLocal = () => {
+    if (localStorage.getItem('theme') === Theme.LIGHT || localStorage.getItem('theme') === null) {
+        addLightTheme(),
+       checkboxSwitchThemeRef.checked = false;
+    } else if(localStorage.getItem('theme') === Theme.DARK) {addDarkTheme(), checkboxSwitchThemeRef.checked = true;
+}
+};
+
+/* Вешаем событие поиска сохраненной на локалке темы */
+document.addEventListener('DOMContentLoaded', loadThemeLocal);
+
+/* добавление-удаление класса*/
+const toggleInThemeDark = () => { addDarkTheme(),removeLightTheme() };
+const toggleInThemeLight = () => { addLightTheme(),removeDarkTheme() };
+const addLightTheme = () => { bodyRef.classList.add(Theme.LIGHT) };
+const addDarkTheme = () => { bodyRef.classList.add(Theme.DARK) };
+const removeLightTheme = () => { bodyRef.classList.remove(Theme.LIGHT) };
+const removeDarkTheme = () => { bodyRef.classList.remove(Theme.DARK) };
+
